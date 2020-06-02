9fe569eecfb17d383bef636078b090b006f5db62
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-06-02 14:49:16 -0700

    initial commit

---
+++ moody-weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moody-weather
+//
+//  Created by Bo on 6/2/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
