50d2889d9e703071e1d42de2b8178b36541bd741
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-06-03 00:06:11 -0700

    connect UI elements

--- moody-weather/ViewController.swift
+++ moody-weather/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var happyButton: UIButton!
+    @IBOutlet weak var neutralButton: UIButton!
+    @IBOutlet weak var sadButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
